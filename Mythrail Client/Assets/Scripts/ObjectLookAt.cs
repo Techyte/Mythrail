ce9725e2753958868f438e2f4a45cfaada804d79
Techyte <owinterh@icloud.com>
2022-10-11 19:48:19 +1100

    Might have fixed camera breaking

--- Mythrail Client/Assets/Scripts/ObjectLookAt.cs
+++ Mythrail Client/Assets/Scripts/ObjectLookAt.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace MythrailEngine
@@ -11,16 +10,7 @@ namespace MythrailEngine
         {
             if(target)
             {
-                Vector3 dir = target.position - transform.position;
-                Debug.Log(dir.sqrMagnitude);
-                if(dir.sqrMagnitude > 0.000001)
-                {
-                    Quaternion lookRot = Quaternion.LookRotation(dir);
-                    lookRot.x = 0;
-                    lookRot.z = 0;
-                    transform.rotation = Quaternion.Slerp(transform.rotation, lookRot,
-                        Mathf.Clamp01(3.0f * Time.maximumDeltaTime));
-                }
+                transform.LookAt(transform.position + target.forward);
             }
         }
     }
